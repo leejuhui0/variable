b8ad2c076675e74e20b906e0ad265b816ddaee40
leejuhui0 <wistua003@naver.com>
2025-02-07 12:04:03 +0900

    첫번째 커밋

---
+++ 01.variable/src/variable/Example01.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		String a = "hello world!!";
+		
+		System.out.println(a);
+		System.out.println(a);
+		System.out.println(a);
+	}
+
+}
